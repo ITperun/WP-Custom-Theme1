accf8310661d72ce1d78857fe0f5cd7e828a8740
Illja <144803015+ITperun@users.noreply.github.com>
2025-01-06 14:22:06 +0100

    create other files

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+?>
+    <footer>
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?></p>
+    </footer>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <h1><?php bloginfo( 'name' ); ?></h1>
+        <h2><?php bloginfo( 'description' ); ?></h2>
+    </header>
